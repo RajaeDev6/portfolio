53c416ed867ea2320322bd9c22a8d96754cda463
rajaeDev6 <rajapple876@gmail.com>
2023-02-28 11:33:35 -0500

    make nav disappear when link click on small screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const style = {
   header: `shadow-md sticky top-0 left-0 w-full z-40 items-center mb-8 bg-black`,
   nav: `abosolute left-0 flex md:mx-auto items-center justify-between max-w-[1200px] w-[90%] p-4`,
   Logo: `font-extrabold text-4xl md:text-5xl text-[#fff] flex items-center`,
-  navlinks: `font-bold md:mr-20 cursor-pointer md:mb-0 md:text-slate-100 hover:text-slate-400`,
+  navlinks: `bg-slate-100 text-black w-full p-4 md:bg-black font-bold md:mr-20 cursor-pointer md:mb-0 md:text-slate-100 lg:hover:text-slate-400`,
   menutoggle: `text-2xl text-slate-100 font-bold cursor-pointer md:hidden`,
 };
 const navlinks = [
@@ -40,40 +40,49 @@ const navlinks = [
 
 const Header: FC = () => {
   const [togglemenu, setToggleMenu] = useState<boolean>(false);
+  const killMenu = () => {
+    setToggleMenu(!togglemenu);
+  };
   const handleToggle = () => {
     setToggleMenu(!togglemenu);
   };
   return (
-    <header className={style.header} style={{boxShadow: "0px 2px 0px rgba(255, 255, 255, 0.20)" }}>
+    <header
+      className={style.header}
+      style={{ boxShadow: "0px 2px 0px rgba(255, 255, 255, 0.20)" }}
+    >
       <div className={style.nav}>
         <div className={style.Logo}>
           <GrCloudComputer size={30} className="text-slate-200 mr-2" />
           <h1>R</h1>
         </div>
-        <ul className={`absolute flex flex-col duration-300 ease-in  w-[100%] md:w-0 md:static ${togglemenu ? 'top-[100%]' : 'top-[-400px]'} bg-[#fff]  md:flex-row text-blacck md:flex md:mx-auto`}>
+        <ul
+          className={`absolute left-0 md:gap-x-14 flex flex-col duration-300 ease-in  w-[100%] md:w-0 md:static ${
+            togglemenu ? "top-[100%]" : "top-[-400px]"
+          } bg-[#fff]  md:flex-row text-blacck md:flex md:mx-auto`}
+        >
           {navlinks.map((item) => (
-            <li className=" mx-4 my-8">
-              <Link
-                to={item.url}
-                spy={item.spy}
-                smooth={item.smooth}
-                offset={item.offset}
-                duration={item.duration}
-                className={style.navlinks}
-              >
-                {item.text}
-              </Link>
-            </li>
+            <Link
+              to={item.url}
+              spy={item.spy}
+              onClick={killMenu}
+              smooth={item.smooth}
+              offset={item.offset}
+              duration={item.duration}
+              className={style.navlinks}
+            >
+              {item.text}
+            </Link>
           ))}
         </ul>
-          {!togglemenu ? (
-            <GiHamburgerMenu
-              onClick={handleToggle}
-              className={style.menutoggle}
-            />
-          ) : (
-            <FaTimes onClick={handleToggle} className={style.menutoggle} />
-          )}
+        {!togglemenu ? (
+          <GiHamburgerMenu
+            onClick={handleToggle}
+            className={style.menutoggle}
+          />
+        ) : (
+          <FaTimes onClick={handleToggle} className={style.menutoggle} />
+        )}
       </div>
     </header>
   );
